ac3530cfb61719bb5dd3e299a8b779244bb6eebe
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-05-20 08:47:24 +0300

    update

--- js/cookie.js
+++ js/cookie.js
@@ -4,6 +4,8 @@ const cookieButton = document.querySelector(".cookie-btn");
 cookieButton.addEventListener("click", () => {
   cookieContainer.classList.remove("active");
   localStorage.setItem("cookieBannerDisplayed", "true");
+  localStorage.setItem("nunta", "fotograf");
+  localStorage.setItem("nunta", "camera");
 });
 
 setTimeout(() => {
