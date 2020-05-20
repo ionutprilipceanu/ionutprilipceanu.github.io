1828cb382f4f52850cb7dcde13368692a29aede7
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-05-20 09:01:47 +0300

    update

--- js/cookie.js
+++ js/cookie.js
@@ -18,28 +18,6 @@ setTimeout(() => {
 
 
 
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("nunta", "fotograf");
-});
-
-setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
-  }
-}, 2000);
-
-
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("nunta", "mireasa");
-});
-
-setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("nunta");
-  }
-}, 2000);
 
 cookieButton.addEventListener("click", () => {
   cookieContainer.classList.remove("active");
