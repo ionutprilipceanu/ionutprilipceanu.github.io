86302b32ece3ae1450aa5268893da21aad12140f
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-06-02 19:51:16 +0300

    update

---
+++ js/loadingBarII.js
@@ -0,0 +1,19 @@
+paceOptions = {
+        ajax: true,
+        document: true,
+        eventLag: false
+        };
+
+        Pace.on('done', function() {
+        $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000,);
+
+
+        $('#preloader').delay(1500).animate({top: '-100%'}, 2000,);
+
+        TweenMax.from(".title", 2, {
+             delay: 1.8,
+                  y: 10,
+                  opacity: 0,
+                  ease: Expo.easeInOut
+            })
+       });
